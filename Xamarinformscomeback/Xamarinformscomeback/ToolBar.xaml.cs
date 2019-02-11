318aaf70f7b2b2d8b9a4a5f9fd3216a0359e24c2
Humphryshikunzi <39647238+Humphryshikunzi@users.noreply.github.com>
2019-02-11 16:44:46 +0300

    Add files via upload
    THese two files were added later

---
+++ Xamarinformscomeback/Xamarinformscomeback/ToolBar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarinformscomeback
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ToolBar : ContentPage
+	{
+		public ToolBar ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ToolbarItem_OnActivated(object sender, EventArgs e)
+        {
+            // Icon is missing ,will update it when I get it
+        }
+    }
+}
