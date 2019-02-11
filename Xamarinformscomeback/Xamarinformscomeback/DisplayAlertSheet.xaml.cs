2ab7a51104db41dee9f44d021e8552973229eb99
Humphryshikunzi <39647238+Humphryshikunzi@users.noreply.github.com>
2019-02-11 16:48:09 +0300

    Add files via upload
    This too was added today

---
+++ Xamarinformscomeback/Xamarinformscomeback/DisplayAlertSheet.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarinformscomeback
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DisplayAlertSheet : ContentPage
+	{
+		public DisplayAlertSheet ()
+		{ 
+			InitializeComponent ();
+		}
+
+        private void Sheet(object sender, EventArgs e)
+        {
+            DisplayActionSheet("Select", "Cancel", "Delete", "Make Phone Call",
+                "Copy link", "SendEmail", "SendMessage", "Save link");
+        }
+
+        private void  Alert3(object sender, EventArgs e)
+        {
+            DisplayAlert("Select", "Contact", "cancel");
+        }
+
+        private void  Alert4(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Contact", "Okay", "Cancel");
+        }
+    }
+}
